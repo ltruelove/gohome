3d2ae585de93f3737f13ae8cced2e516dabf1843
ltruelove <levitruelove@gmail.com>
2021-09-01 16:53:01 -0400

    Add history to room temperatures

--- web/js/temps.js
+++ web/js/temps.js
@@ -4,12 +4,17 @@ function TempsViewModel(){
     this.master = {"humidity":ko.observable(), "celcius":ko.observable(), "fahrenheit": ko.observable() };
     this.garage = {"humidity":ko.observable(), "celcius":ko.observable(), "fahrenheit": ko.observable() };
     this.main = {"humidity":ko.observable(), "celcius":ko.observable(), "fahrenheit": ko.observable() };
+    this.kidsHistory = [];
+    this.masterHistory = [];
+    this.garageHistory = [];
+    this.mainHistory = [];
     
     this.getGarageStatus = function(){
         $.get('/garage', function(response){
             self.garage.humidity(response.humidity);
             self.garage.celcius(response.celcius);
             self.garage.fahrenheit(response.fahrenheit);
+            self.garageHistory.push(response);
         });
     }
     
@@ -18,6 +23,7 @@ function TempsViewModel(){
             self.kids.humidity(response.humidity);
             self.kids.celcius(response.celcius);
             self.kids.fahrenheit(response.fahrenheit);
+            self.kidsHistory.push(response);
         });
     }
     
@@ -26,6 +32,7 @@ function TempsViewModel(){
             self.master.humidity(response.humidity);
             self.master.celcius(response.celcius);
             self.master.fahrenheit(response.fahrenheit);
+            self.masterHistory.push(response);
         });
     }
     
@@ -34,6 +41,7 @@ function TempsViewModel(){
             self.main.humidity(response.humidity);
             self.main.celcius(response.celcius);
             self.main.fahrenheit(response.fahrenheit);
+            self.mainHistory.push(response);
         });
     }
 
