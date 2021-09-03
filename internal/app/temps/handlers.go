4eaba904814615e20ff719cf569c10d4fd58ef34
ltruelove <levitruelove@gmail.com>
2021-09-03 08:04:04 -0400

    Add endpoint for fetching all room temperatures

--- internal/app/temps/handlers.go
+++ internal/app/temps/handlers.go
@@ -1,6 +1,7 @@
 package temps
 
 import (
+	"encoding/json"
 	"fmt"
 	"html/template"
 	"io/ioutil"
@@ -13,13 +14,21 @@ import (
 
 var Config config.Configuration
 
+type RoomTemperature struct {
+	Fahrenheit float32 `json:"fahrenheit"`
+	Celcius    float32 `json:"celcius"`
+	Humidity   float32 `json:"humidity"`
+	Name       string  `json:"name"`
+}
+
 func RegisterHandlers(mainConfig config.Configuration) {
 	Config = mainConfig
 	routing.AddGenericRoute("/temps", TempsHandler)
-	routing.AddGenericRoute("/kids", HandleKidsSettingsRequest)
-	routing.AddGenericRoute("/garage", HandleGarageSettingsRequest)
-	routing.AddGenericRoute("/master", HandleMasterSettingsRequest)
-	routing.AddGenericRoute("/main", HandleMainSettingsRequest)
+	routing.AddGenericRoute("/temps/kids", HandleKidsSettingsRequest)
+	routing.AddGenericRoute("/temps/garage", HandleGarageSettingsRequest)
+	routing.AddGenericRoute("/temps/master", HandleMasterSettingsRequest)
+	routing.AddGenericRoute("/temps/main", HandleMainSettingsRequest)
+	routing.AddGenericRoute("/temps/all", HandleAllSettingsRequest)
 }
 
 func TempsHandler(writer http.ResponseWriter, request *http.Request) {
@@ -44,8 +53,59 @@ func makeRequest(writer http.ResponseWriter, request *http.Request, address stri
 		panic(rErr)
 	}
 
+	writeResponse(writer, responseData)
+}
+
+func writeResponse(writer http.ResponseWriter, responseData []byte) {
 	writer.Header().Set("Content-Type", "application/json")
 	writer.Write(responseData)
+}
+
+func fetchTemperature(address string, name string) RoomTemperature {
+	status, err := http.Get(address)
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer status.Body.Close()
+	responseData, rErr := ioutil.ReadAll(status.Body)
+
+	if rErr != nil {
+		panic(rErr)
+	}
+
+	var t RoomTemperature
+
+	err = json.Unmarshal(responseData, &t)
+	if err != nil {
+		panic(err)
+	}
+
+	t.Name = name
+
+	return t
+}
+
+func HandleAllSettingsRequest(writer http.ResponseWriter, request *http.Request) {
+	allTemps := make([]RoomTemperature, 0)
+	kidsTemp := fetchTemperature(fmt.Sprintf("http://%s", Config.KidsStatusIP), "Kids' Room")
+	mainTemp := fetchTemperature(fmt.Sprintf("http://%s", Config.MainStatusIP), "Main Floor")
+	masterTemp := fetchTemperature(fmt.Sprintf("http://%s", Config.MasterStatusIP), "Master Bedroom")
+	garageTemp := fetchTemperature(fmt.Sprintf("http://%s", Config.GarageStatusIP), "Garage")
+
+	allTemps = append(allTemps, kidsTemp)
+	allTemps = append(allTemps, mainTemp)
+	allTemps = append(allTemps, masterTemp)
+	allTemps = append(allTemps, garageTemp)
+
+	result, err := json.Marshal(allTemps)
+
+	if err != nil {
+		panic(err)
+	}
+
+	writeResponse(writer, result)
 }
 
 func HandleKidsSettingsRequest(writer http.ResponseWriter, request *http.Request) {
--- web/js/temps.js
+++ web/js/temps.js
@@ -1,62 +1,22 @@
 function TempsViewModel(){
     var self = this;
-    this.kids = {"humidity":ko.observable(), "celcius":ko.observable(), "fahrenheit": ko.observable() };
-    this.master = {"humidity":ko.observable(), "celcius":ko.observable(), "fahrenheit": ko.observable() };
-    this.garage = {"humidity":ko.observable(), "celcius":ko.observable(), "fahrenheit": ko.observable() };
-    this.main = {"humidity":ko.observable(), "celcius":ko.observable(), "fahrenheit": ko.observable() };
+    this.rooms = ko.observable([]);
     this.kidsHistory = [];
     this.masterHistory = [];
     this.garageHistory = [];
     this.mainHistory = [];
     
-    this.getGarageStatus = function(){
-        $.get('/garage', function(response){
-            self.garage.humidity(response.humidity);
-            self.garage.celcius(response.celcius);
-            self.garage.fahrenheit(response.fahrenheit);
-            self.garageHistory.push(response);
+    this.getRoomStatus = function(){
+        $.get('/temps/all', function(response){
+            self.rooms(response);
         });
-    }
-    
-    this.getKidsStatus = function(){
-        $.get('kids', function(response){
-            self.kids.humidity(response.humidity);
-            self.kids.celcius(response.celcius);
-            self.kids.fahrenheit(response.fahrenheit);
-            self.kidsHistory.push(response);
-        });
-    }
-    
-    this.getMasterStatus = function(){
-        $.get('/master', function(response){
-            self.master.humidity(response.humidity);
-            self.master.celcius(response.celcius);
-            self.master.fahrenheit(response.fahrenheit);
-            self.masterHistory.push(response);
-        });
-    }
-    
-    this.getMainStatus = function(){
-        $.get('/main', function(response){
-            self.main.humidity(response.humidity);
-            self.main.celcius(response.celcius);
-            self.main.fahrenheit(response.fahrenheit);
-            self.mainHistory.push(response);
-        });
-    }
-
-    this.fetchResults = function(){
-        self.getGarageStatus();
-        self.getKidsStatus();
-        self.getMasterStatus();
-        self.getMainStatus();
     }
 
     $(window).on("load", function(){
-        self.fetchResults();
+        self.getRoomStatus();
 
         setInterval(function(){
-            self.fetchResults();
+            self.getRoomStatus();
         }, 10000);
     });
 }
